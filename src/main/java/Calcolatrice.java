53f31b2b4302f0d8c0b8751c8c8beada760e680c
LeonardoSartini <s8901875p@majorana-giorgi.edu.it>
2025-01-13 09:50:10 +0100

    asdasd

--- src/main/java/Calcolatrice.java
+++ src/main/java/Calcolatrice.java
@@ -6,8 +6,47 @@ public class Calcolatrice
 		double s=0;
 		for(double addendo:addendi)
 		{
-			s=s+addendo;
+			if(addendo < 0)
+			{
+				System.out.println("addendo negativo");
+			}
+			if(addendo == 0)
+			{
+				System.out.println("addendo : 0");
+			}
+			if(addendo > 0)
+			{
+				System.out.println("addendo è positivo");
+			}
+			s=s+ addendo;
 		}
 		return s;
+	}
+	
+	public double differenza (double d1, double d2)
+	{
+		double diff=0;
+		diff = d1-d2;
+		return diff;
+	}
+	public double moltiplicazione(double...moltiplicandi)
+	{
+		double s=1;
+		for(double moltiplicando:moltiplicandi)
+		{
+			s=s*moltiplicando;
+		}
+		return s;
+	}
+	
+	public double divisione(double divisore, double dividendo) throws Exception
+	{
+		double ris;
+		ris = divisore/dividendo;
+		if(dividendo==0)
+		{
+			throw new Exception();
+		}
+		return ris;
 	}
 }
